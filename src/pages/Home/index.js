52be55405513596fa44773045bb0461e4b1f764f
mauriani <maurianim@gmail.com>
2020-12-30 12:21:47 -0300

    atualizacoes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -37,6 +37,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -53,7 +54,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#fff" /> 3
+                <MdAddShoppingCart size={16} color="#fff" />
+                {amount[product.id] || 0}
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
@@ -64,8 +66,16 @@ class Home extends Component {
     );
   }
 }
+
+const mapstateToProps = (state) => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {}),
+});
 // converte actions em propriedades
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapstateToProps, mapDispatchToProps)(Home);
